4727e1b6097e4b032e5b48d3d9f8c1725d49935d
Tran Vu Tuan <tranvutuan2001@gmail.com>
2026-02-08 23:46:05 +0100

    add transit engine

--- bootstraps/init-secret.sh
+++ bootstraps/init-secret.sh
@@ -4,3 +4,4 @@ set -euo pipefail
 kubectl apply --server-side --force-conflicts -f ./secret/tls-secret.yaml
 kubectl apply --server-side --force-conflicts -f ./secret/db-user-secret.yaml
 kubectl apply --server-side --force-conflicts -f ./secret/openbao-postgres-secret.yaml
+kubectl apply --server-side --force-conflicts -f ./secret/openbao-transit-token.yaml
