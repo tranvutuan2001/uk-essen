3264d6a785139921da4dad2ba6656d4e6cd60c34
Tran Vu Tuan <tranvutuan2001@gmail.com>
2026-02-09 00:20:36 +0100

    feat: update transit engine configuration and remove unused transit token secret

--- bootstraps/init-secret.sh
+++ bootstraps/init-secret.sh
@@ -4,4 +4,3 @@ set -euo pipefail
 kubectl apply --server-side --force-conflicts -f ./secret/tls-secret.yaml
 kubectl apply --server-side --force-conflicts -f ./secret/db-user-secret.yaml
 kubectl apply --server-side --force-conflicts -f ./secret/openbao-postgres-secret.yaml
-kubectl apply --server-side --force-conflicts -f ./secret/openbao-transit-token.yaml
